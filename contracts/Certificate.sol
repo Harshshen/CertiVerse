8dc8d82946bb15ed561ee4e7de7c11b4938d8d30
Kundan <100273989+InfiniteCoder100@users.noreply.github.com>
2024-02-03 10:20:00 +0530

    Sixth Commit

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -73,4 +73,85 @@ contract XRC721 is ERC721URIStorage {
         return idToListedToken[tokenId];
     }
 
+    function getCurrentToken() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    //this function create the NFT when organisation click on button
+    function createToken(
+        string memory tokenURI,
+        address receiver
+    ) public payable returns (uint256) {
+        _tokenIds.increment();
+        uint256 currentTokenId = _tokenIds.current();
+        _safeMint(msg.sender, currentTokenId);
+        _setTokenURI(currentTokenId, tokenURI);
+
+        createListedToken(currentTokenId, receiver);
+
+        return currentTokenId;
+    }
+
+    // set the information about certificate and transfer certificate to user
+    function createListedToken(uint256 tokenId, address receiver) private {
+        idToListedToken[tokenId] = ListedToken(
+            tokenId,
+            payable(msg.sender),
+            payable(receiver),
+            false
+        );
+
+        _transfer(msg.sender, receiver, tokenId);
+    }
+
+    // return array of all the certificate created by organisation
+    function getOrganisationCertificates(
+        address org
+    ) public view returns (ListedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (org == idToListedToken[i + 1].creator) itemCount++;
+        }
+
+        ListedToken[] memory items = new ListedToken[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (org == idToListedToken[i + 1].creator) {
+                uint currentId = i + 1;
+                ListedToken storage currentItem = idToListedToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
+
+    //For user side
+    //Return array of all the certificates owned by User
+    function getUserCertificates(
+        address user
+    ) public view returns (ListedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (user == idToListedToken[i + 1].user) itemCount++;
+        }
+
+        ListedToken[] memory items = new ListedToken[](itemCount);
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (user == idToListedToken[i + 1].user) {
+                uint currentId = i + 1;
+                ListedToken storage currentItem = idToListedToken[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex++;
+            }
+        }
+        return items;
+    }
 }
+
+
